5c64a7733c29c973436ac334926b7fb1926b4b45
Jonathan Dyallo <emeraldusk@gmail.com>
2022-10-21 20:19:07 -0300

    vite prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "MyLib",
+      fileName: (format) => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
     outDir: "build",
   },
   server: {
     open: true,
+    port: 3500,
+  },
+  preview: {
     port: 3500,
   },
 });
