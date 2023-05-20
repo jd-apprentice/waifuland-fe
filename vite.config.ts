bb60de896587db3384d0d31299eaf3e569603dc8
Jonathan Dyallo <emeraldusk@gmail.com>
2023-05-20 18:14:09 -0300

    fix(vite): more configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,15 @@ export default defineConfig({
         viteTsconfigPaths(),
         svgrPlugin(),
     ],
+    resolve: {
+        alias: [
+            { find: /^~/, replacement: '' },
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src'),
+            },
+        ],
+    },
     build: {
         outDir: 'dist',
     },
