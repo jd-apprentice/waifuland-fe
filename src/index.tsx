8c1dee75067f04e40c4a393d06ee6474ec4e63d0
jd-apprentice <contacto@jonathan.com.ar>
2025-02-02 22:22:13 -0300

    re-deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import { render } from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react'
-import App from './App'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import App from './App';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-render(
-    <ChakraProvider>
+root.render(
+    <ChakraProvider value={defaultSystem}>
         <App />
     </ChakraProvider>,
-    document.getElementById('root')
 );
 
