0d4422d3a32b5cd9d005a968bbbc13fb3262866c
jd-apprentice <contacto@jonathan.com.ar>
2025-02-02 23:06:19 -0300

    pin versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
+import { render } from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react'
-import App from './App';
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container!);
+import App from './App'
 
-root.render(
+render(
     <ChakraProvider>
         <App />
     </ChakraProvider>,
+    document.getElementById('root')
 );
-
