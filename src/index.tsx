7d77827afa4e87fe505b28ccae0e6d6af3ec0cb8
jd-apprentice <contacto@jonathan.com.ar>
2025-02-02 22:23:58 -0300

    default system not works

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
         <App />
     </ChakraProvider>,
 );
